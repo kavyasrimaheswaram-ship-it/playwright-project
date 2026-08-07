3dec1d2f8e096f371c1a62f8e105c57c900d687d
chaitanya4b2 <51979181+chaitanya4b2@users.noreply.github.com>
2026-08-07 13:59:56 +0400

    Headlessmode

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
      baseURL:"https://ecommerce-playground.lambdatest.io/index.php?",
-    headless: false,
+    headless: true,
     screenshot: "on",
     video: "retain-on-failure",
  
